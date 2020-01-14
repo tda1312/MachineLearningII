d436741aadfab9b4ce0da6aed1bb4017c37501a5
tda1312 <tda1312@gmail.com>
2020-01-14 18:38:36 +0700

    implement PCA

--- K-means/k-means.py
+++ K-means/k-means.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 # load dataset
 file_data = "../Datasets/Iris/iris.data"
@@ -14,21 +15,51 @@ target = ['target']
 x = df.loc[:, features].values
 y = df.loc[:, target].values
 
+# find the optimum number of cluster with Elbow Method
 wcss = []
 for i in range(1, 11):
     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
     kmeans.fit(x)
     wcss.append(kmeans.inertia_)
+
+# plot Elbow Method
 plt.plot(range(1, 11), wcss)
 plt.title('Elbow Method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
 plt.show()
 
+# categorize the data
 kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
 pred_y = kmeans.fit_predict(x)
 
+# plot K-means
 plt.scatter(x[:, 0], x[:, 1])
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='red')
 plt.title('K-means')
+plt.show()
+
+# standardize the data for PCA
+x = StandardScaler().fit_transform(x)
+
+# 'dimension reduction' PCAE projection to 2D
+pca = PCA(n_components = 2)
+principalComponents = pca.fit_transform(x)
+principalDf = pd.DataFrame(data = principalComponents, columns = ['PC 1', 'PC 2'])
+finalDf = pd.concat([principalDf, df[['target']] ], axis = 1)
+
+# visualize 2D projection
+fig = plt.figure(figsize = (8, 8))
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('Principal Component Analysis', fontsize = 20)
+
+targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets, colors):
+    indicesToKeep = finalDf['target'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'PC 1'], finalDf.loc[indicesToKeep, 'PC 2'], c = color, s = 50)
+ax.legend(targets)
+ax.grid()
 plt.show()
